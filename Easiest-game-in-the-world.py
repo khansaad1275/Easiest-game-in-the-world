523cfadc4f7888128c302d25c5c112c63284c3aa
khan saad <32749921+khansaad1275@users.noreply.github.com>
2020-05-06 17:27:21 +0530

    Update Easiest-game-in-the-world.py

--- Easiest-game-in-the-world.py
+++ Easiest-game-in-the-world.py
@@ -1,3 +1,5 @@
+from time import sleep
+import sys
 import time
 points = 0
 wronganswers = 0
@@ -14,10 +16,10 @@ print(" -----------------------")'''
 
 # Question 1
 print("1) Enter Your Name ")
-ans = input("Enter Your Answer: ")
+Name = input("Enter Your Answer: ")
 
 print(" ____________________")
-print("< Hello!, "+ ans+"    >")
+print("< Hello!, "+ Name+"    >")
 print(" -------------------- ")
 print("      \   ^__^        ")
 print("       \  (oo)\_______")
@@ -26,7 +28,7 @@ print("              ||----w |")
 print("              ||     ||")
 print("-----------------------")
 
-time.sleep(3)
+time.sleep(1)
 
 
 
@@ -35,7 +37,24 @@ time.sleep(3)
 print(" ")
 print("2:Press The Enter Button")
 print("")
+print("""\
+
+███████╗███╗   ██╗████████╗███████╗██████╗ 
+██╔════╝████╗  ██║╚══██╔══╝██╔════╝██╔══██╗
+█████╗  ██╔██╗ ██║   ██║   █████╗  ██████╔╝
+██╔══╝  ██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗
+███████╗██║ ╚████║   ██║   ███████╗██║  ██║
+╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+                                           
+
+          """)
+print("")
 ans2 = input("Enter Your Answer: ")
+if(ans2 == ""):
+     points+=1
+else:
+     wronganswers+=1
+
 
 
 
@@ -46,8 +65,8 @@ ans2 = input("Enter Your Answer: ")
 print(" ")
 print("3: ?siHT daeR ruoY naC")
 print("(a)No\n(b)Yes\n(c)I am Sleeping\n(d)i hate this game\n")
-ans2 = input("Enter Your Answer: ")
-if(ans2 == "b"):
+anss = input("Enter Your Answer: ")
+if(anss == "b"):
      points+=1
 else:
      wronganswers+=1
@@ -56,10 +75,9 @@ else:
 
 
 
-# Question 3
+# Question 4
 print(" ")
-
-print("3: Guess the Contry Flag")
+print("4: Guess the Contry Flag")
 print("")
 print(" _________")
 print("|::_,^._::|")
@@ -77,12 +95,62 @@ else:
 
 
 
-# Question 4
-print("4) Can you see THIS?")
-print("(a)Yes\n(b)No\n")
+# Question 5
+print("5) Can you see THIS?")
+print("(a)Yes\n(b)No\n(c)Probably\n(d)Who Cares?\n")
 ans = input("Enter Your Answer: ")
 if(ans == "a"):
      points+=1
+else:
+     wronganswers+=1
+
+
+
+# Question 6
+print("6)")
+print("""\
+          ^         
+         | |        
+       @#####@      
+     (###   ###)-.  
+   .(###     ###) \ 
+  /  (###   ###)   )
+ (=-  .@#####@|_--"      This is a hold up! Put all your money in the
+ /\    \_|l|_/ (\        disk drive slot, and no one gets hurt!
+(=-\     |l|    /   
+ \  \.___|l|___/    
+ /\      |_|   /    
+(=-\._________/\    
+ \             /    
+   \._________/     
+     #  ----  #     
+     #   __   #       
+     \########/      
+
+         V
+             V
+           V
+     """)
+print("(a)Yes\n(b)No\n(c)I WON'T\n(d)ok,ok I am putting the Money But Please Don't Hurt me")
+ans = input("Enter Your Answer: ")
+if(ans == "a" or "d"):
+     points+=2
+
+
+
+     thanks = "ĹМŦĂŐ XD, 𝕿𝖍𝖆𝖓𝖐𝖘   " + Name
+     youaregetting = "You are Getting One Extra Point For It ^_^"
+     for x in thanks:
+          print(x, end='')
+          sys.stdout.flush()
+          sleep(0.1)
+     print("")
+     for x in youaregetting:
+          print(x, end='')
+          sys.stdout.flush()
+          sleep(0.1)
+
+
 else:
      wronganswers+=1
 
